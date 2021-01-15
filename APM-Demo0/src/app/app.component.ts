965f2e4c29e29e2c6276fb6e592a3abd93dbfa52
Rex Zulfekar <rex.zulfekar@viacom.com>
2021-01-15 14:41:47 -0500

    comment added to test.

--- APM-Demo0/src/app/app.component.ts
+++ APM-Demo0/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+// comment
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent {}
